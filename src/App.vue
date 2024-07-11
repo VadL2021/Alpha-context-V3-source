8f314ad1d0f1b7a19c7b27fe57e626669205d1cb
VadL2021 <161335743+VadL2021@users.noreply.github.com>
2024-07-11 05:00:33 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HeaderView class="header"/>
+  <MainView class="main"/>
+  <FooterView class="footer"/>
+</template>
+
+<script>
+import MainView from "./common.blocks/main/main.vue"
+import FooterView from "./common.blocks/footer/footer.vue"
+import HeaderView from "./common.blocks/header/header.vue"
+
+export default {
+  name: 'App',
+  components: {
+    MainView,
+    FooterView,
+    HeaderView
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "./App";
+</style>
